3986aab8eb6bd2922f6cf4bdf1623d7edc7949f9
Hucci <im.huccihuang@gmail.com>
2026-03-01 23:35:33 +0800

    暂时移除fastapi部分，以避免混淆

--- fastapi_app/db/base.py
+++
@@ -1,7 +0,0 @@
-"""SQLAlchemy Base 定义"""
-
-from sqlalchemy.orm import DeclarativeBase
-
-
-class Base(DeclarativeBase):
-    pass
--- fastapi_app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from fastapi_app.models.conversation import Conversation
-from fastapi_app.models.message import Message
-from fastapi_app.models.user import User
-
-__all__ = ["User", "Message", "Conversation"]
